2d32b2c5df5a0ca4b1206675df8911166020292c
Your Name <you@example.com>
2017-04-06 03:10:42 -0400

    trying something

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-function Question(theQuestion,theWrongAnswers,theAnswer){
+
+function Question (theQuestion, theAnswer) {
   this.question=theQuestion;
-  this.=theWrongAnswers;
   this.answer=theAnswer;
-  this.isCorrect= function(){}
 
   }
+  function multiChoiceQuestion (theQuestion, theAnswer,theChoices)
+  {
+    Question.call(this, theQuestion,theAnswer, theChoices);
+    this.theChoices=theChoices;
+  }
+  multiChoiceQuestion.prototype=Object.create(Question.prototype);
+
+  multiChoiceQuestion.prototype.multiChoice=function()
+  {
+    let li = event.target;
+    let answerSpace = li.parentElement.nextElementSibling;
+    if (li.textContent === this.theAnswer) {
+      answerSpace.textContent = "Correct!";
+    } else {
+      answerSpace.textContent = "Try Again.";
+    }
+  }
+
+
+
+
+  this.display = function () {
+    let source = document.querySelector('#question').innerHTML;
+    let template = Handlebars.compile(source);
+    let html = template(this);
+    document.querySelector('#quiz').insertAdjacentHTML('beforeend', html);
+    document.querySelector('#quiz article:last-of-type ul').addEventListener('click', this.isCorrect.bind(this));
+  }
 }
-let wrong  = [];
-let right =[];
-let rightanswer = new Question();
-let reacherCast = ['Tom Cruise', 'Rosamund Pike', 'Richard Jenkins', 'David Oyelowo', 'Werner Herzog'];
-let jackReacher = new Movie('Jack Reacher', 'big', reacherCast);
+let question1 = new Question('The keyword or the property that you use to refer to an object through which they were invoked is ', ['from', 'to', 'this', 'object'], 'this');
+let question2 = new Question('The behaviour of the instances present of a class inside a method is defined by', ['Method', 'Classes', 'Interfaces', 'Classes and Interfaces'], 'Classes');
+[question1, question2].forEach(question => question.display());
